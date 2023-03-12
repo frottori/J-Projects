040ccebb1eb16cbae3ae29c6001a1692a1b21c42
Frosso Varsou <119513126+frottori@users.noreply.github.com>
2023-03-12 13:30:53 +0200

    Update and rename Calc.java to CalcV1.java

--- Calculator/v1/CalcV1.java
+++ Calculator/v1/CalcV1.java
@@ -2,13 +2,13 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class Calc extends JFrame implements ActionListener {
+public class CalcV1 extends JFrame implements ActionListener {
 
     CalcPad b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,mult,sub,addt,eq,cl,sign,div,perc,comm;
     JTextField text;
     String num1,oper,num2;
     
-    Calc() {
+    CalcV1() {
         oper = num1 = num2 = "";
         b0 = new CalcPad("0",140,50);
         b1 = new CalcPad("1",70,50);
@@ -158,6 +158,6 @@ public class Calc extends JFrame implements ActionListener {
         }
     }
     public static void main (String[] args) {
-        new Calc();
+        new CalcV1();
     }
 }
